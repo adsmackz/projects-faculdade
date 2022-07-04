80c186573ebaf3ef48cfe1015886651dc61f5ee3
mackz <90354303+adsmackz@users.noreply.github.com>
2022-07-04 11:57:52 -0300

    Update mapa.c

--- Logica/mapa.c
+++ Logica/mapa.c
@@ -1,33 +1,14 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <locale.h>
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <string.h>
 
 int main(){
-    setlocale(LC_ALL,"");
-    int D, d, q, r;
-
-    printf("Qual é o valor que você deseja dividir: ");
-    scanf("%d", &D);
-    printf("Por quanto vocẽ deseja dividir: ");
-    scanf("%d", &d);
-
-    q = D / d;     
-    r = D % q;
-  
-    printf("\n.................................");
-    if (r == 0){
-       printf("\nA divisão é exata, então: ");
-       printf("\nQuociente: %d.", q);
-       printf("\nE o Resto é: 0.");
-      
-    }  else {
-       printf("\nA divisão não é exata, então: ");
-       printf("\nQuociente: %d.", q);
-       printf("\nE o Resto é: %d.", r);
-
-    }
-    printf("\n.................................");
-    return 0;
-
-
 }
+
+	
+		
+    
+
+
+ 
+	
